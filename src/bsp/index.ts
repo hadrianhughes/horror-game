66723ac46ccb0534870efcd9a84c937c45a11623
Hadrian Hughes <hadrianhughes@gmail.com>
2022-08-28 22:42:04 +0100

    Remove _dot variable and use its value directly

--- src/bsp/index.ts
+++ src/bsp/index.ts
@@ -59,9 +59,7 @@ export const isPlayerInNode = ([px, py]: Vector, vertices: Vector[]): boolean =>
     const pi: Vector = [px - ix, py - iy]
     const pj: Vector = [px - jx, py - jy]
 
-    const _dot = dotProduct(pi, pj)
-
-    const innerAngle = Math.acos(_dot / (magnitude(pi) * magnitude(pj)))
+    const innerAngle = Math.acos(dotProduct(pi, pj) / (magnitude(pi) * magnitude(pj)))
     sum += innerAngle
   }
 
