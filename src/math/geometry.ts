310312fcda33cbbb06160cb19ee95e7f5da61306
Hadrian Hughes <hadrianhughes@gmail.com>
2022-09-12 11:14:35 +0100

    Fix quadrant bug causing wrong walls to render

--- src/math/geometry.ts
+++ src/math/geometry.ts
@@ -7,13 +7,18 @@ export const rad = (degs: number): number => degs * (Math.PI / 180)
 export const destructVec = (v: vec2): [number, number] => [v[0], v[1]]
 
 // Starting from the vertical
-export enum Quadrant { Q1, Q2, Q3, Q4 }
+export enum Quadrant {
+  Q1 = 'q1',
+  Q2 = 'q2',
+  Q3 = 'q3',
+  Q4 = 'q4',
+}
 
 export const quadrant = (v: vec2): Quadrant => {
   const [x, y] = destructVec(v)
 
-  if (x <= 0 && y > 0) return Quadrant.Q1
-  if (x <= 0 && y <= 0) return Quadrant.Q2
-  if (x > 0 && y <= 0) return Quadrant.Q3
-  if (x > 0 && y > 0) return Quadrant.Q4
+  if (x >= 0 && y >= 0) return Quadrant.Q1
+  if (x < 0 && y >= 0) return Quadrant.Q2
+  if (x < 0 && y < 0) return Quadrant.Q3
+  if (x >= 0 && y < 0) return Quadrant.Q4
 }
